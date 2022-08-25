48f4007cf6bbfe815b3674bee60319a46be674de
guiifigueiredo <guilherme_figueiredo@outlook.com.br>
2022-08-25 15:34:21 -0400

    promises with async/await

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ function wrongType(err){
     throw new Error(chalk.red(err.code, 'nenhum arquivo selecionado'))
 }
 
-function getFile(path){
-    const encoding = 'utf-8'
-    fs.promises
-        .readFile(path, encoding)
-        .then((text) => console.log(chalk.green(text)))
-        .catch((err) => wrongType(err))
+async function getFile(path){
+    const encoding = 'utf-8'    
+    try {
+        const text = await fs.promises.readFile(path, encoding)
+        console.log(chalk.green(text))
+    } catch (err){
+        wrongType(err)
+    }
 }
+
 console.log(getFile('./arquivos/texto1.md'))
