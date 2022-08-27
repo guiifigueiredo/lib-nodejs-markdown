3f274edc3bff8da1bd9479f11427c4609eda71a3
guiifigueiredo <guilherme_figueiredo@outlook.com.br>
2022-08-26 20:13:12 -0400

    code with regular expression

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+function allLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const resul = [];
+    let i;
+
+    while((i = regex.exec(texto)) !== null){
+        resul.push({ [i[1]]: i[2] });
+    }
+
+    return resul;
+}
+
 function wrongType(err){
     throw new Error(chalk.red(err.code, 'nenhum arquivo selecionado'))
 }
@@ -9,9 +21,11 @@ async function getFile(path){
     const encoding = 'utf-8'    
     try {
         const text = await fs.promises.readFile(path, encoding)
-        console.log(chalk.green(text))
+        console.log(allLinks(text))
     } catch (err){
         wrongType(err)
+    } finally {
+        console.log(chalk.blue("Operação concluída"))
     }
 }
 
